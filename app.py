2ab071d7ebe9f1f793acd71207afb7440b30df6a
Pilar Rodriguez Perez <40452218@live.napier.ac.uk>
2021-11-25 10:54:55 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template ('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template ('contact.html')
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
