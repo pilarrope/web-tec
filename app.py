7e0d622636dcdb889c86db4191c917ab7e897e55
Pilar Rodriguez Perez <40452218@live.napier.ac.uk>
2021-12-09 21:24:00 +0000

    my website

--- app.py
+++ app.py
@@ -1,16 +1,86 @@
+from flask import Flask, render_template, request
 
-from flask import Flask, render_template
+
+from forms import ContactForm
+import pandas as pd
+
 app = Flask(__name__)
+app.secret_key = 'secretKey'
+
 
 @app.route('/home')
 def home():
     return render_template ('index.html')
 
-@app.route('/contact')
-def contact():
-    return render_template ('contact.html')
+
+@app.route('/recipes')
+def recipes():
+    return render_template ('recipes.html')
+
+@app.route('/tips')
+def tips():
+    return render_template ('tips.html')
+
+@app.route('/about')
+def about():
+    return render_template ('about.html')
+
+@app.route('/accessibility')
+def accessibility():
+    return render_template ('accessibility.html')
 
 
+@app.route('/recipe')
+def recipe():
+    return render_template ('recipe.html')
+
+
+@app.route('/croquetas')
+def croquetas():
+    return render_template ('croquetas.html')
+
+@app.route('/cupcakes')
+def cupcakes():
+    return render_template ('cupcakes.html')
+
+@app.route('/toast')
+def toast():
+    return render_template ('toast.html')
+
+@app.route('/paella')
+def paella():
+    return render_template ('paella.html')
+
+@app.route('/chococake')
+def chococake():
+    return render_template ('chococake.html')
+
+@app.route('/chickensalad')
+def chickensalad():
+    return render_template ('chickensalad.html')
+
+
+
+
+
+
+
+@app.route('/contact', methods=["GET","POST"])
+def get_contact():
+    form = ContactForm()
+    # here, if the request type is a POST we get the data on contact
+    #forms and save them else we return the contact forms html page
+    if request.method == 'POST':
+        name =  request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+        res = pd.DataFrame({'name':name, 'email':email, 'subject':subject ,'message':message}, index=[0])
+        res.to_csv('./contactusMessage.csv')
+        return render_template('contact.html', form=form)
+    else:
+        return render_template('contact.html', form=form)
+	
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
